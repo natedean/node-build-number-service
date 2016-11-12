3457b9b93dd99d21c6e8ae30fbf5fd0a8524c886
Nate Dean <nate.dean@pathar.net>
2016-11-12 09:50:30 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const http = require('http');
+
+let counter = 150;
+
+let reqHandler = (req, res) => {
+	counter = counter + 1;
+	console.log(`I incremented the counter, yo. ${counter}`);
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.write(JSON.stringify(counter));
+	res.end();
+}
+
+const server = http.createServer(reqHandler);
+
+server.listen(3000);
